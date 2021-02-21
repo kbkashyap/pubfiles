bb94286dd2a613c503aba329c47b03a1de028638
kbkashyap <51267952+kbkashyap@users.noreply.github.com>
2021-02-22 00:39:42 +0530

    update img dwnload

--- runImageDwnld.ps1
+++ runImageDwnld.ps1
@@ -1,1 +1,3 @@
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Set-Executionpolicy -Scope currentuser remotesigned
 Invoke-WebRequest -Uri https://github.com/kbkashyap/pubfiles/raw/main/SampleImage.png -OutFile "C:\Users\Administrator\Desktop\Sample.png"
